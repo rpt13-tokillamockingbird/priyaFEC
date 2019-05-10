90ea66c4fb40ae23661928f7ecb976bf43b25062
Priyadubal18 <priya.dubal18@gmai.com>
2019-05-10 00:27:25 -0400

    Have the create table script

--- server/database/index.js
+++ server/database/index.js
@@ -15,6 +15,36 @@ dbConnection.connect(function (err) {
     }
     console.log("Connected to MySQL server FEC Database")
 
+    let createSizeTable = `CREATE TABLE if not exists Size(
+                            SizeId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                            USSize varchar(50),
+                            EUSize varchar(50)
+                        )`;
+
+    let createWidthTable = `CREATE TABLE if not exists Width(
+                            WidthId int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+                            WidthType varchar(50)
+                        )`;
+
+
+    let createColorTable = `CREATE TABLE if not exists Color(
+                            ColorId int NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+                            ColorsName  varchar(50),
+                            Color varchar(50)
+                        )`;
+
+    let createProductInventoryTable = `CREATE TABLE if not exists ProductInventory (
+                                       RecId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                       ProductId int, 
+                                       Quantity int, 
+                                       SizeId  int,
+                                       WidthId int, 
+                                       ColorId  int,
+                                       FOREIGN KEY (SizeId) REFERENCES Size (SizeId),
+                                       FOREIGN KEY (WidthId) REFERENCES Width (WidthId),
+                                       FOREIGN KEY (ColorId) REFERENCES Color(ColorId),
+                                    )`;
+
     // Create Tables if not exist
 });
 
