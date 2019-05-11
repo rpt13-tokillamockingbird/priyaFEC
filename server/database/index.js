01157e5fa5122759306f8770179557f8e5c7d9bc
Priyadubal18 <priya.dubal18@gmai.com>
2019-05-10 21:53:45 -0400

    Create database table

--- server/database/index.js
+++ server/database/index.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 // and to the database "chat".
 dbConnection = mysql.createConnection({
     user: 'root',
-    password: '',
+    password: 'rootPriya1211',
     database: 'fec'
 });
 
@@ -20,18 +20,33 @@ dbConnection.connect(function (err) {
                             USSize varchar(50),
                             EUSize varchar(50)
                         )`;
+    dbConnection.query(createSizeTable, function (err, results, fields) {
+        if (err) {
+            console.log('createSizeTable:', err.message);
+        }
+    });
 
     let createWidthTable = `CREATE TABLE if not exists Width(
                             WidthId int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
                             WidthType varchar(50)
                         )`;
-
+    dbConnection.query(createWidthTable, function (err, results, fields) {
+        if (err) {
+            console.log('createWidthTable:', err.message);
+        }
+    });
 
     let createColorTable = `CREATE TABLE if not exists Color(
                             ColorId int NOT NULL  AUTO_INCREMENT PRIMARY KEY,
                             ColorsName  varchar(50),
                             Color varchar(50)
                         )`;
+
+    dbConnection.query(createColorTable, function (err, results, fields) {
+        if (err) {
+            console.log('createColorTable', err.message);
+        }
+    });
 
     let createProductInventoryTable = `CREATE TABLE if not exists ProductInventory (
                                        RecId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -42,15 +57,20 @@ dbConnection.connect(function (err) {
                                        ColorId  int,
                                        FOREIGN KEY (SizeId) REFERENCES Size (SizeId),
                                        FOREIGN KEY (WidthId) REFERENCES Width (WidthId),
-                                       FOREIGN KEY (ColorId) REFERENCES Color(ColorId),
+                                       FOREIGN KEY (ColorId) REFERENCES Color(ColorId)
                                     )`;
 
+    dbConnection.query(createProductInventoryTable, function (err, results, fields) {
+        if (err) {
+            console.log('ProductInventory', err.message);
+        }
+    });
     // Create Tables if not exist
+    dbConnection.end(function (err) {
+        if (err) {
+            return console.error('Error in disConnecting Database FEC :' + err);
+        }
+    });
 });
 
 
-dbConnection.end(function (err) {
-    if (err) {
-        return console.error('Error in disConnecting Database FEC :' + err);
-    }
-});
