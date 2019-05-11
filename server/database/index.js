2b16a4fc12e17af845afe9a307e3690856240aac
Priyadubal18 <priya.dubal18@gmai.com>
2019-05-10 23:55:51 -0400

    Adds color data in color table

--- server/database/index.js
+++ server/database/index.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var faker = require('faker');
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
@@ -14,6 +15,31 @@ dbConnection.connect(function (err) {
         return console.error('Error in connecting Database FEC :' + err);
     }
     console.log("Connected to MySQL server FEC Database")
+    let deleteProductInventoryTable = `DROP TABLE IF EXISTS ProductInventory`;
+    dbConnection.query(deleteProductInventoryTable, function (err, results, fields) {
+        if (err) {
+            console.log('deleteProductInventoryTable:', err.message);
+        }
+    });
+    let deleteSizeTable = `DROP TABLE IF EXISTS Size`;
+    dbConnection.query(deleteSizeTable, function (err, results, fields) {
+        if (err) {
+            console.log('deleteSizeTable:', err.message);
+        }
+    });
+    let deleteWidthTable = `DROP TABLE IF EXISTS Width`;
+    dbConnection.query(deleteWidthTable, function (err, results, fields) {
+        if (err) {
+            console.log('deleteWidthTable:', err.message);
+        }
+    });
+    let deleteColorTable = `DROP TABLE IF EXISTS Color`;
+    dbConnection.query(deleteColorTable, function (err, results, fields) {
+        if (err) {
+            console.log('deleteColorTable:', err.message);
+        }
+    });
+
 
     let createSizeTable = `CREATE TABLE if not exists Size(
                             SizeId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -25,6 +51,8 @@ dbConnection.connect(function (err) {
             console.log('createSizeTable:', err.message);
         }
     });
+
+
 
     let createWidthTable = `CREATE TABLE if not exists Width(
                             WidthId int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
@@ -48,6 +76,8 @@ dbConnection.connect(function (err) {
         }
     });
 
+
+
     let createProductInventoryTable = `CREATE TABLE if not exists ProductInventory (
                                        RecId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
                                        ProductId int, 
@@ -59,13 +89,30 @@ dbConnection.connect(function (err) {
                                        FOREIGN KEY (WidthId) REFERENCES Width (WidthId),
                                        FOREIGN KEY (ColorId) REFERENCES Color(ColorId)
                                     )`;
-
     dbConnection.query(createProductInventoryTable, function (err, results, fields) {
         if (err) {
             console.log('ProductInventory', err.message);
         }
     });
-    // Create Tables if not exist
+
+    var colorArray = [];
+    for (let i = 0; i <= 50; i++) {
+        colorArray.push(faker.commerce.color())
+    }
+    let uniqueArray = [...new Set(colorArray)]
+    uniqueArray.forEach(color => {
+        if (color.split(" ").length < 2) {
+            let inserColorStmt = `INSERT INTO Color (ColorsName, Color) VALUE ('${color}', '${color}')`;
+            dbConnection.query(inserColorStmt, (err, results, fields) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                console.log('Color Id:' + results.insertId);
+            });
+        }
+    });
+
+
     dbConnection.end(function (err) {
         if (err) {
             return console.error('Error in disConnecting Database FEC :' + err);
