d72cdd18bc7f39c6c7c00ef6f1c6ca35b23af375
Priyadubal18 <priya.dubal18@gmai.com>
2019-05-15 23:25:30 -0400

    Undo node modules

--- client/src/components/ProductBuyer.jsx
+++ client/src/components/ProductBuyer.jsx
@@ -7,16 +7,39 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Container';
 
+const productInfo = {
+    fontFamily: "Arial",
+    lineHeight: '1.4',
+    color: '#393939',
+    width: '25%'
+}
 const reviewNumberStyle = {
     display: 'inline-block',
-    fontFamily: 'Brandon Text',
-    fontWeight: '400',
-    lineHeight: '1.4',
     fontSize: '1.3rem',
     position: 'relative',
     bottom: '5px'
 };
 
+const productTitle = {
+    fontSize: '1.3rem',
+};
+
+const productSubTitle = {
+    fontSize: '1.1rem',
+};
+const productPrice = {
+    fontSize: '1rem',
+    lineHeight: '2',
+    fontWeight: '600'
+}
+
+const strikePrice = {
+    fontWeight: '400'
+}
+
+const productDesc = {
+    fontSize: '0.8rem',
+}
 
 
 export default class ProductBuyer extends React.Component {
@@ -32,11 +55,11 @@ export default class ProductBuyer extends React.Component {
         debugger;
         let productServiceData = {
             productTitle: 'Everett Plain Toe Derby',
-            prodcutSubTitle: 'THE RAIL',
+            productSubTitle: 'THE RAIL',
             productOriginalPrice: 99.94,
-            productDiscountPrice: 99.94,
+            productDiscountPrice: 89.94,
             productDiscountPercent: 33,
-            prodcutSingleLinDesc: "Burnished fine-grain leather and precision-stitched welting add polished versatility to an essential shoe for every man's wardrobe."
+            productSingleLinDesc: "Burnished fine-grain leather and precision-stitched welting add polished versatility to an essential shoe for every man's wardrobe."
         }
 
         let reviewServiceData = {
@@ -63,17 +86,41 @@ export default class ProductBuyer extends React.Component {
     }
 
     render() {
+        const isDiscounted = this.state.productInfo.productDiscountPrice;
+        let productPriceInfo;
+        if (isDiscounted) {
+            productPriceInfo = <p>$<strike style={strikePrice}>{this.state.productInfo.productOriginalPrice}</strike>&nbsp;${this.state.productInfo.productDiscountPrice}&nbsp;  {this.state.productInfo.productDiscountPercent}% off</p>
+        }
+        else {
+            productPriceInfo = <p>${this.state.productInfo.productOriginalPrice}</p>
+        }
         return (
-            <div >
-                <h1>The product buyer Component</h1>
-                <StarRatingComponent
-                    name="productRate"
-                    starCount={5}
-                    starColor="black"
-                    emptyStarColor="grey"
-                    value={this.state.productReviewInfo.productRating}
-                />
-                <span style={reviewNumberStyle}>({this.state.productReviewInfo.productNumberOfRating})</span>
+            <div style={productInfo}>
+                <h3>NORDSTORM Product Buyer</h3>
+                <div id="prdReview">
+                    <StarRatingComponent
+                        name="productRate"
+                        starCount={5}
+                        starColor="black"
+                        emptyStarColor="grey"
+                        value={this.state.productReviewInfo.productRating}
+                    />
+                    <span style={reviewNumberStyle}>({this.state.productReviewInfo.productNumberOfRating})</span>
+                </div>
+                <div id="prdInfo">
+                    <div style={productTitle}>
+                        {this.state.productInfo.productTitle}
+                    </div>
+                    <div style={productSubTitle}>
+                        {this.state.productInfo.productSubTitle}
+                    </div>
+                    <div style={productPrice}>
+                        {productPriceInfo}
+                    </div>
+                    <p style={productDesc}>
+                        {this.state.productInfo.productSingleLinDesc}
+                    </p>
+                </div>
 
             </div>
         )
