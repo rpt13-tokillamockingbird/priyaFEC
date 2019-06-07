446ab2f493c9c53ba915ed1febb96d0750a74080
Priyadubal18 <priya.dubal18@gmai.com>
2019-06-06 23:53:29 -0400

    Fixed Jest Enzyem testing

--- client/src/components/ProductBuyer.jsx
+++ client/src/components/ProductBuyer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import StarRatingComponent from 'react-star-rating-component';
 import Dropdown from 'react-dropdown'
-import 'react-dropdown/style.css'
-import dropDownStyle from '../../src/style/DropDown.css';
+import "react-dropdown/style.css"
+import dropDownStyle from "../../src/style/DropDown.css";
 import swal from 'sweetalert';
 
 const productInfo = {
--- server/spec/ProductBuyer.spec.js
+++ server/spec/ProductBuyer.spec.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ProductBuyer from '../../client/src/components/ProductBuyer';
+
+// describe('ProductBuyer', () => {
+//     it('should check div ProductBuyer is present', () => {
+//         const component = shallow(<ProductBuyer />);
+//         console.log(component);
+//         expect(component.find('div.ProductBuyer')).to.have.lenghtOf(1);
+//     });
+// });
+
 describe('ProductBuyer', () => {
-    it('should check div ProductBuyer is present', () => {
-        const component = shallow(<ProductBuyer />);
-        expect(component.find('div.ProductBuyer')).to.have.lenghtOf(1);
+    it('should render correctly in "debug" mode', () => {
+        const component = shallow(<ProductBuyer debug />);
+        expect(component).toMatchSnapshot();
     });
 });
