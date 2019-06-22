2caf7a486ee7c5c63398a3d582db4072b006efdc
Priyadubal18 <priya.dubal18@gmai.com>
2019-06-22 03:11:51 -0400

    Use a new port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const port = 3010;
 const dbConnect = require("../server/database/getBuyerInfo");
 const path = require('path');
 var proxy = require('http-proxy-middleware');
